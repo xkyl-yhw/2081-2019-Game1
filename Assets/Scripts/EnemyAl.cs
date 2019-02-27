3b21e142a30b24509464eac1aaa393f5f68593ea
xkyl-yhw <xkylhh@163.com>
2019-02-27 15:41:53 +0800

    2-27-15-41

---
+++ Assets/Scripts/EnemyAl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAl : MonoBehaviour {
+
+    private bool enemyPassive = false;
+    private bool enemyPositive = false;
+    private bool enemyDefensive = false;
+    private int enemyHealth = 100;
+    public bool playerPosx = false;
+    public bool playerPosy = false;
+    private bool isblocked = false;
+    private RaycastHit hit;
+    private Transform playerTran;
+	void Start () {
+        playerTran = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+    void backWard()
+    {
+        this.transform.Translate((this.transform.position - playerTran.position) * Time.deltaTime);
+    }
+    void shooting()
+    {
+
+    }
+	void Update () {
+        Debug.DrawLine(transform.position, playerTran.position, Color.red, 1f);
+        isblocked =Physics.Linecast(this.transform.position, playerTran.position, 1 << LayerMask.NameToLayer("Player"));
+        if (!isblocked)
+        {
+            isblocked = false;
+        }
+        else isblocked = true;
+        Debug.Log(isblocked);
+        if (Vector3.Distance(this.transform.position, playerTran.position) < 5f)
+        {
+            playerPosx = true;
+        }
+        else playerPosx = false;
+        if (Vector3.Distance(this.transform.position, playerTran.position) < 4f)
+        {
+            playerPosy = true;
+        }
+        else playerPosy = false;
+        if (enemyHealth == 100 && !playerPosx && !playerPosy)
+        {
+            enemyPassive = true;
+            enemyPassive = false;
+            enemyDefensive = false;
+        }
+        if ((playerPosx == true||playerPosy == true) && isblocked == false)
+        {
+            enemyPositive = true;
+            enemyPassive = false;
+            enemyDefensive = false;
+            shooting();
+        }
+        if (playerPosy == true||enemyHealth<=20)
+        {
+            enemyDefensive = true;
+            enemyPassive = false;
+            enemyPositive = false;
+            backWard();
+        }
+	}
+}
---
+++ Assets/Scripts/playerColltroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerColltroller : MonoBehaviour {
+
+    public GameObject bullet;
+    private Transform playerTran;
+	void Start () {
+        playerTran = this.GetComponent<Transform>();
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerTran.Translate(Vector3.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTran.Translate(-Vector3.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTran.Translate(Vector3.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTran.Translate(-Vector3.left * Time.deltaTime);
+        }
+    }
+}
