5b3d6d231cd0d3afeb65b3a82126402236d111a4
xkyl-yhw <xkylhh@163.com>
2019-02-28 11:53:27 +0800

    2-28-11-53

--- Assets/Scripts/EnemyAl.cs
+++ Assets/Scripts/EnemyAl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAl : MonoBehaviour {
 
@@ -11,27 +12,50 @@ public class EnemyAl : MonoBehaviour {
     public bool playerPosx = false;
     public bool playerPosy = false;
     private bool isblocked = false;
+    private bool ishit = false;
     private RaycastHit hit;
     private Transform playerTran;
-	void Start () {
+    private NavMeshAgent m_agent;
+    private GameObject[] barrier;
+    private GameObject targetBarrier;
+    public float speed=5f;
+    void Start () {
+        barrier = GameObject.FindGameObjectsWithTag("barrier");
         playerTran = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        m_agent = this.GetComponent<NavMeshAgent>();
+        FindNearest();
 	}
+    void FindNearest()
+    {
+        float minDis=Vector3.Distance(this.transform.position,barrier[0].GetComponent<Transform>().position);
+        foreach(GameObject x in barrier)
+        {
+            if (Vector3.Distance(this.transform.position, x.GetComponent<Transform>().position) < minDis)
+            {
+                minDis = Vector3.Distance(this.transform.position, x.GetComponent<Transform>().position);
+                targetBarrier = x;
+            }
+        }
+    }
     void backWard()
     {
-        this.transform.Translate((this.transform.position - playerTran.position) * Time.deltaTime);
+        if (this.transform.position == targetBarrier.transform.position+new Vector3(0.5f,0.5f,0.5f)) return;
+        m_agent.SetDestination(targetBarrier.GetComponent<Transform>().position+new Vector3(0.5f, 0.5f, 0.5f));
+        m_agent.Move(this.transform.TransformDirection(new Vector3(0, 0, speed * Time.deltaTime)));
     }
     void shooting()
     {
 
     }
 	void Update () {
-        Debug.DrawLine(transform.position, playerTran.position, Color.red, 1f);
-        isblocked =Physics.Linecast(this.transform.position, playerTran.position, 1 << LayerMask.NameToLayer("Player"));
-        if (!isblocked)
+        ishit = Physics.Raycast(this.transform.position, (playerTran.position-this.transform.position), out hit);
+        if (ishit)
         {
-            isblocked = false;
+            if (hit.collider.CompareTag("Player"))
+            {
+                isblocked = true;
+            }else isblocked = false;
         }
-        else isblocked = true;
         Debug.Log(isblocked);
         if (Vector3.Distance(this.transform.position, playerTran.position) < 5f)
         {
@@ -61,6 +85,7 @@ public class EnemyAl : MonoBehaviour {
             enemyDefensive = true;
             enemyPassive = false;
             enemyPositive = false;
+            if (this.transform.position == targetBarrier.transform.position + new Vector3(0.5f, 0.5f, 0.5f)) return;
             backWard();
         }
 	}
